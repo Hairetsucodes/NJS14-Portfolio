dfef61aa6b1c66cff63a0567414822bcd7e08fc6
Hairetsu <Hairetsu@gmail.com>
2024-04-15 15:43:30 -0700

    fixed tailwind items-center class in sites-section.tsx

--- components/navigation/nav-menu.tsx
+++ components/navigation/nav-menu.tsx
@@ -41,7 +41,9 @@ export function NavMenu() {
                         </NavigationMenuItem>
                         <NavigationMenuItem>
                             <NavigationMenuTrigger className={'border'}>
+                                <Link href={"/portfolio"} legacyBehavior passHref>
                                 Portfolio
+                                </Link>
                             </NavigationMenuTrigger>
                             <NavigationMenuContent>
                                 <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
