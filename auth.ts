4bcfc6d70e60e9a466553988cce0ac67b4cf52ee
Hairetsu <Hairetsu@gmail.com>
2024-04-16 13:16:02 -0700

    O Authorization Integration for Google and Github

--- auth.ts
+++ auth.ts
@@ -14,6 +14,14 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
+    events: {
+        async linkAccount({user}) {
+            await db.user.update({
+                where: {id: user.id},
+                data: {emailVerified: new Date()}
+            })
+        }
+    },
     callbacks: {
         async session({token, session}) {
             if (token.sub && session.user) {
