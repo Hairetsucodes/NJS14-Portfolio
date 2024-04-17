f9fcfbb3777db37076fea44791a5ee7bc96563ae
Hairetsu <Hairetsu@gmail.com>
2024-04-17 15:52:05 -0700

    opengraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {Toaster} from '@/components/ui/Toast'
-import {cn} from "@/lib/utils";
-import {NavMenu} from "@/components/navigation/nav-menu";
-import {Footer} from "@/components/footer/Footer";
-import {auth, signOut} from '@/auth'
-const inter = Inter({subsets: ["latin"]});
+import { Toaster } from '@/components/ui/Toast'
+import { cn } from "@/lib/utils";
+import { NavMenu } from "@/components/navigation/nav-menu";
+import { Footer } from "@/components/footer/Footer";
+import { auth, signOut } from '@/auth'
+const inter = Inter({ subsets: ["latin"] });
 
 
 export const metadata: Metadata = {
+    openGraph: {
+        title: 'Hairetsu.com',
+        description: 'Built with Open Portfolio by Hairetsu'
+    },
     title: "Hairetsu.com",
-    description: "build with Open Portfolio by Hairetsu",
+    description: "Built with Open Portfolio by Hairetsu",
 };
 
 export default async function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     const session: string | any = await auth()
@@ -35,22 +39,22 @@ export default async function RootLayout({
 
     return (
         <html lang="en">
-        <body
-            className={`min-h-screen h-full w-full bg-black dark:bg-grid-small-blue-500 overflow-x-hidden bg-grid-small-blue-500 ${inter.className}`}>
-        <header
-            className={cn("dark fixed top-0 z-50 flex h-16 w-screen shrink-0 justify-between border-b  bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl")}>
-            <div className={'w-full flex justify-center items-center'}>
-                <NavMenu session={session} >
-                    <SignOutButton/>
-                </NavMenu>
-            </div>
-        </header>
-        <div className={cn("pt-16 ")}>
-            {children}
-        </div>
-        <Toaster/>
-        <Footer/>
-        </body>
+            <body
+                className={`min-h-screen h-full w-full bg-black dark:bg-grid-small-blue-500 overflow-x-hidden bg-grid-small-blue-500 ${inter.className}`}>
+                <header
+                    className={cn("dark fixed top-0 z-50 flex h-16 w-screen shrink-0 justify-between border-b  bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl")}>
+                    <div className={'w-full flex justify-center items-center'}>
+                        <NavMenu session={session} >
+                            <SignOutButton />
+                        </NavMenu>
+                    </div>
+                </header>
+                <div className={cn("pt-16 ")}>
+                    {children}
+                </div>
+                <Toaster />
+                <Footer />
+            </body>
         </html>
     );
 }
