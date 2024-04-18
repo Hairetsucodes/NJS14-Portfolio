b9e08663f42855d75322d5fbad00073b188f6772
Hairetsu <Hairetsu@gmail.com>
2024-04-17 17:04:24 -0700

    added postHog analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { cn } from "@/lib/utils";
 import { NavMenu } from "@/components/navigation/nav-menu";
 import { Footer } from "@/components/footer/Footer";
 import { auth, signOut } from '@/auth'
+import { CSPostHogProvider } from './providers'
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -39,22 +42,24 @@ export default async function RootLayout({
 
     return (
         <html lang="en">
-            <body
-                className={`min-h-screen h-full w-full bg-black dark:bg-grid-small-blue-500 overflow-x-hidden bg-grid-small-blue-500 ${inter.className}`}>
-                <header
-                    className={cn("dark fixed top-0 z-50 flex h-16 w-screen shrink-0 justify-between border-b  bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl")}>
-                    <div className={'w-full flex justify-center items-center'}>
-                        <NavMenu session={session} >
-                            <SignOutButton />
-                        </NavMenu>
+            <CSPostHogProvider>
+                <body
+                    className={`min-h-screen h-full w-full bg-black dark:bg-grid-small-blue-500 overflow-x-hidden bg-grid-small-blue-500 ${inter.className}`}>
+                    <header
+                        className={cn("dark fixed top-0 z-50 flex h-16 w-screen shrink-0 justify-between border-b  bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl")}>
+                        <div className={'w-full flex justify-center items-center'}>
+                            <NavMenu session={session} >
+                                <SignOutButton />
+                            </NavMenu>
+                        </div>
+                    </header>
+                    <div className={cn("pt-16 ")}>
+                        {children}
                     </div>
-                </header>
-                <div className={cn("pt-16 ")}>
-                    {children}
-                </div>
-                <Toaster />
-                <Footer />
-            </body>
+                    <Toaster />
+                    <Footer />
+                </body>
+            </CSPostHogProvider>
         </html>
     );
 }
