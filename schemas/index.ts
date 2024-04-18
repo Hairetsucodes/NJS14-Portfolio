3ccd2c8f183feb026d5d63749dcee7d7047da1eb
Hairetsu <Hairetsu@gmail.com>
2024-04-18 15:08:08 -0700

    oops

--- schemas/index.ts
+++ schemas/index.ts
@@ -15,7 +15,7 @@ export const RegisterSchema = z.object({
     email: z.string().email("Email is required"),
     password: z.string().min(8, "Password is required"),
     name: z.string().min(1, "Name is required"),
-    username: z.string().min(1, "Username is required")ß
+    username: z.string().min(1, "Username is required")
 })
 
 export const NewBlogSchema = z.object({
