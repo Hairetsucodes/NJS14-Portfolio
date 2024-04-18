41bd63c2749ff48031f8fd9e2899182c65694728
Hairetsu <Hairetsu@gmail.com>
2024-04-18 12:54:15 -0700

    update author page

--- components/author/author-blog-page.tsx
+++ components/author/author-blog-page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import Image from 'next/image';
 import Link from 'next/link';
-import {Button} from "@/components/ui/button";
-import {deletePost} from "@/data/blog";
-import {useRouter} from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { deletePost } from "@/data/blog";
+import { useRouter } from "next/navigation";
 
 interface BlogPageProps {
     posts: {
@@ -19,9 +19,9 @@ interface BlogPageProps {
     }[] | null,
 }
 
-export default function AuthorBlogPage({posts} : BlogPageProps) {
+export default function AuthorBlogPage({ posts }: BlogPageProps) {
     const router = useRouter();
-    if (posts == null)  {
+    if (posts == null) {
         return null
     }
 
@@ -34,7 +34,7 @@ export default function AuthorBlogPage({posts} : BlogPageProps) {
     return (
         <div className="min-h-screen p-5">
             <main>
-                <section className="mb-10">
+                <section className="m-4">
                     <div className="flex justify-between items-center mb-4">
                         <h1 className="text-3xl font-bold">Blog Posts</h1>
                         <Link href={"/create-post"}>
@@ -43,64 +43,99 @@ export default function AuthorBlogPage({posts} : BlogPageProps) {
                     </div>
                     <div className="flex flex-col md:flex-row gap-4">
                         {posts.length > 0 && (
-                            <article className="bg-card border rounded-lg p-6 md:w-2/3">
-                                <div className="w-full flex justify-center items-center">
-                                    <Image
-                                        src="/planner.png"
-                                        alt="Most Recent Blog Post"
-                                        width={700}
-                                        height={400}
-                                        className="rounded-md border mt-8"
-                                    />
-                                </div>
-                                <h2 className="text-2xl text-center font-semibold mt-4">{posts[0].title}</h2>
-                                <p className="text-gray-600 text-center font-semibold">
-                                    {new Date(posts[0].date).toLocaleDateString('en-US', {
-                                        year: 'numeric',
-                                        month: 'long',
-                                        day: 'numeric',
-                                    })}
-                                </p>
+                            <article
+                                className="bg-card border rounded-lg  md:w-2/3 flex  items-center justify-center">
+                                <div className="p-4">
+                                    <div className="w-full h-full flex">
+                                        <div>
+                                            <Link href={`/blog/${posts[0].slug}`}>
+                                                <Image
+                                                    src="/planner.png"
+                                                    alt="Most Recent Blog Post"
+                                                    width={1280}
+                                                    height={800}
+                                                    className="rounded-md border "
+                                                />
+                                            </Link>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <Link href={`/blog/${posts[0].slug}`}>
+                                            <h2 className="text-2xl text-center pt-4 font-semibold">{posts[0].title}</h2>
+                                        </Link>
+                                        <Link href={`/blog/${posts[0].slug}`}>
+                                            <p className="text-gray-600 text-center font-semibold">
+                                                {new Date(posts[0].date).toLocaleDateString('en-US', {
+                                                    year: 'numeric',
+                                                    month: 'long',
+                                                    day: 'numeric',
+                                                })}
+                                            </p>
+                                        </Link>
+                                    </div>
 
-                                <div className="flex flex-col justify-center items-center mt-4">
-                                    <div className="flex justify-center items-center">
-                                        <Link href={`/edit-post/${posts[0].slug}`}>
-                                            <Button variant="secondary" className="mr-2">
-                                                Edit
+                                    <div>
+                                        <p className="text-gray-600 text-center font-semibold">
+                                            {posts[0].content.split('\n')[2]}
+                                        </p>
+                                    </div>
+                                    <div className="flex flex-col justify-center items-center mt-4">
+                                        <div className="flex justify-center items-center">
+                                            <Link href={`/edit-post/${posts[0].slug}`}>
+                                                <Button variant="secondary" className="mr-2">
+                                                    Edit
+                                                </Button>
+                                            </Link>
+                                            <Button variant="destructive" onClick={() => handleDelete(posts[0].id)}>
+                                                Delete
+                                            </Button>
+                                        </div>
+                                        <Link href={`/blog/${posts[0].slug}`}>
+                                            <Button variant="secondary" className="mt-2">
+                                                Read More
                                             </Button>
                                         </Link>
-                                        <Button variant="destructive" onClick={() => handleDelete(posts[0].id)}>
-                                            Delete
-                                        </Button>
                                     </div>
-                                    <Link href={`/blog/${posts[0].slug}`}>
-                                        <Button variant="secondary" className="mt-2">
-                                            Read More
-                                        </Button>
-                                    </Link>
                                 </div>
                             </article>
                         )}
-                        <div className="md:w-1/3 grid grid-cols-1 gap-4">
+                         <div className="md:w-1/3 grid grid-cols-1 gap-4">
                             {posts.slice(1, 3).map((post, index) => (
                                 <article key={index} className="bg-card border text-center rounded-lg p-4">
-                                    <div className="w-full flex justify-center">
-                                        <Image
-                                            src="/comp.png"
-                                            alt="Runner Up Blog Post"
-                                            width={300}
-                                            height={200}
-                                            className="rounded-md border"
-                                        />
+                                    <div className={'w-full flex  items-center justify-center'}>
+                                        <Link
+                                            href={`/blog/${post.slug}`}
+                                        >
+                                            <Image
+                                                src="/comp.png"
+                                                alt="Runner Up Blog Post"
+                                                width={850}
+                                                height={550}
+                                                className="rounded-md border"
+                                            />
+                                        </Link>
                                     </div>
-                                    <h2 className="text-xl font-semibold mt-2">{post.title}</h2>
-                                    <p className="text-gray-600 text-center font-semibold">
-                                        {new Date(post.date).toLocaleDateString('en-US', {
-                                            year: 'numeric',
-                                            month: 'long',
-                                            day: 'numeric',
-                                        })}
-                                    </p>
+                                    <div className={'pb-2'}>
+                                        <Link
+                                            href={`/blog/${post.slug}`}
+                                        >
+                                            <h2 className="text-xl font-semibold pt-2">{post.title}</h2>
+                                        </Link>
+
+                                        <Link
+                                            href={`/blog/${post.slug}`}
+                                        >
+                                            <p className="text-gray-600 text-center font-semibold">
+
+                                                {new Date(post.date).toLocaleDateString('en-US', {
+                                                    year: 'numeric',
+                                                    month: 'long',
+                                                    day: 'numeric',
+                                                })}
+                                            </p>
+                                        </Link>
+                                    </div>
+
                                     <div className="flex flex-col justify-center items-center mt-4">
                                         <div className="flex justify-center items-center">
                                             <Link href={`/edit-post/${post.slug}`}>
@@ -126,7 +161,7 @@ export default function AuthorBlogPage({posts} : BlogPageProps) {
                 <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {posts.slice(3).map((post, index) => (
                         <article key={index}
-                                 className="bg-card border text-center rounded-lg p-6 flex flex-col justify-between h-full">
+                            className="bg-card border text-center rounded-lg p-6 flex flex-col justify-between h-full">
                             <Image
                                 src="/comp.png"
                                 alt="Blog Post"
